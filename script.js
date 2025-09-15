39723528dc342e7a05ee51c227e49fea94611c6a
Ishika Verma <ishikaverma2317@gmail.com>
2025-09-15 08:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ class MinHeap {
   constructor() {
     this.heap = [];
   }
-  
+
   insert(val) {
     this.heap.push(val);
     this.bubbleUp();
   }
-  
+
   bubbleUp() {
     let idx = this.heap.length - 1;
     while (idx > 0) {
@@ -17,7 +17,7 @@ class MinHeap {
       idx = parentIdx;
     }
   }
-  
+
   extractMin() {
     if (this.heap.length === 1) return this.heap.pop();
     const min = this.heap[0];
@@ -25,51 +25,43 @@ class MinHeap {
     this.sinkDown(0);
     return min;
   }
-  
+
   sinkDown(idx) {
-    let left = 2 * idx + 1;
-    let right = 2 * idx + 2;
-    let smallest = idx;
-    
-    if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
-      smallest = left;
-    }
-    if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
-      smallest = right;
-    }
-    
-    if (smallest !== idx) {
-      [this.heap[smallest], this.heap[idx]] = [this.heap[idx], this.heap[smallest]];
-      this.sinkDown(smallest);
+    const length = this.heap.length;
+    while (true) {
+      let left = 2 * idx + 1;
+      let right = 2 * idx + 2;
+      let smallest = idx;
+
+      if (left < length && this.heap[left] < this.heap[smallest]) smallest = left;
+      if (right < length && this.heap[right] < this.heap[smallest]) smallest = right;
+
+      if (smallest === idx) break;
+      [this.heap[idx], this.heap[smallest]] = [this.heap[smallest], this.heap[idx]];
+      idx = smallest;
     }
   }
-  
+
   size() {
     return this.heap.length;
   }
 }
 
 function mincost(arr) {
-  let heap = new MinHeap();
-  for (let num of arr) {
-    heap.insert(num);
-  }
-  
-  let totalCost = 0;
-  
+  const heap = new MinHeap();
+  for (let num of arr) heap.insert(num);
+
+  let cost = 0;
   while (heap.size() > 1) {
     let first = heap.extractMin();
     let second = heap.extractMin();
-    
-    let cost = first + second;
-    totalCost += cost;
-    
-    heap.insert(cost);
+    let sum = first + second;
+    cost += sum;
+    heap.insert(sum);
   }
-  
-  return totalCost;
+  return cost;
 }
 
-// 🔹 Example Runs
-console.log(mincost([4, 3, 2, 6]));   // 29
+// Example usage
+console.log(mincost([4, 3, 2, 6])); // 29
 console.log(mincost([1, 2, 3, 4, 5])); // 33
